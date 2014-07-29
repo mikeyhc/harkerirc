90f097e1a92ed189a6454b3acbee4e7d0f763138
Michael Blockley <michael@sss.co.nz>
2014-07-29 16:00:24 +1200

    added auth and list methods

--- src/HarkerIRC/Types.hs
+++ src/HarkerIRC/Types.hs
@@ -1,4 +1,10 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module HarkerIRC.Types where
+
+import Control.Exception
+import Data.List
+import Data.Typeable
 
 type Nick = String
 type User = String
@@ -7,6 +13,38 @@ type Message = String
 
 data IRCMessage = IRCMessage { ircnick :: Nick
                              , ircuser :: User
+                             , ircauth :: Bool
                              , ircchan :: Chan
                              , ircmsg  :: Message
                              }
+nullIRCMessage = IRCMessage "" "" False "" ""
+
+data IRCException = IRCMessageParseException String
+    deriving (Typeable, Show)
+
+instance Exception IRCException
+
+ircMessageToList :: IRCMessage -> [String]
+ircMessageToList (IRCMessage n u a c m) =
+    [ "nick: " ++ n
+    , "user: " ++ c
+    , "auth: " ++ show a
+    , "chan: " ++ c
+    , "msg: "  ++ m
+    ]
+
+mpuException :: String -> IRCException
+mpuException = IRCMessageParseException . (++) "Unrecognized token: "
+
+ircMessageFromList :: [String] -> IRCMessage
+ircMessageFromList = foldl mkmessage nullIRCMessage
+    where
+        mkmessage :: IRCMessage -> String -> IRCMessage
+        mkmessage m s
+            | "nick: " `isPrefixOf` s = m { ircnick = drop 6 s }
+            | "user: " `isPrefixOf` s = m { ircuser = drop 6 s }
+            | "auth: true"  ==      s = m { ircauth = True     }
+            | "auth: false" ==      s = m { ircauth = False    }
+            | "chan: " `isPrefixOf` s = m { ircchan = drop 6 s }
+            | "msg: "  `isPrefixOf` s = m { ircmsg  = drop 5 s }
+            | otherwise               = throw $ mpuException s
